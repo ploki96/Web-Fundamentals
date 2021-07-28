f164ce7b3be7b3714b83ff7dc40cc9e02e90d379
Timothy Wang <timmywang47@yahoo.com>
2021-07-28 14:32:34 -0400

    changed folder name, added gitignore

---
+++ Web Development W2D1/Dojo Pizzeria/pizzeria.js
@@ -0,0 +1,22 @@
+// Create a function called pizzaOven that returns a JavaScript (Pizza) Object
+
+function pizzaFactory(crustType, sauceType, cheeses, toppings) {
+    var pizza = {};
+    pizza.bread = crustType;
+    pizza.protein = sauceType;
+    pizza.cheese = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+
+// Create a pizza with: "deep dish", "traditional", ["mozzarella"], and ["pepperoni", "sausage"]
+
+var pizza1 = pizzaFactory("deep dish", "traditional", ["mozzerella"], ["pepperoni", "sausage"]);
+
+console.log (pizza1);
+
+// Create a pizza with: "hand tossed", "marinara", ["mozzarella", "feta"], and ["mushrooms", "olives", "onions"]
+
+var pizza2 = pizzaFactory("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
+
+console.log(pizza2);
