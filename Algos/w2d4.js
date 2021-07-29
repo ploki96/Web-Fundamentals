e148b56eb5c9d0d7f85630ffd5ac951671ce5ae4
Timothy Wang <timmywang47@yahoo.com>
2021-07-29 11:05:14 -0400

    w2d4 algorithms

---
+++ Algos/w2d4.js
@@ -0,0 +1,13 @@
+function flatten(arr2d) {
+    var flat = [];
+    // your code here
+    for (var i=0;i<arr2d.length;i++) {
+        for(var j=0;j<arr2d[i].length;j++) {
+            flat.push(arr2d[i][j]);
+        }
+    }
+    return flat;
+}
+    
+var result = flatten( [ [2, 5, 8], [3, 6, 1], [5, 7, 7] ] );
+console.log(result);
